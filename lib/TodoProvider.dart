6be4b385823f9336cf50f5a0a926982f5639bb4e
Patte <patrick.voss1808@gmail.com>
2018-11-07 10:24:43 +0100

    Initial commit

---
+++ lib/TodoProvider.dart
@@ -0,0 +1,81 @@
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'package:streamqflite/streamqflite.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+
+
+final String tableTodo = 'todo';
+final String columnId = '_id';
+final String columnTitle = 'title';
+final String columnDone = 'done';
+
+class Todo {
+  int id;
+  String title;
+  bool done;
+
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      columnTitle: title,
+      columnDone: done == true ? 1 : 0
+    };
+    if (id != null) {
+      map[columnId] = id;
+    }
+    return map;
+  }
+
+  Todo();
+
+  Todo.fromMap(Map<String, dynamic> map) {
+    id = map[columnId];
+    title = map[columnTitle];
+    done = map[columnDone] == 1;
+  }
+}
+
+class TodoProvider {
+  Database _db;
+  StreamDatabase _streamDatabase;
+  bool _didInit = false;
+
+  Future _open() async {
+    var databasePath = await getDatabasesPath();
+    String path = join(databasePath, "todo.db");
+    _db = await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+          await db.execute('''
+create table $tableTodo ( 
+  $columnId integer primary key autoincrement, 
+  $columnTitle text not null,
+  $columnDone integer not null)
+''');
+        });
+
+    _streamDatabase = StreamDatabase(_db);
+
+    _didInit = true;
+  }
+
+  Future _init() async {
+    if (!_didInit) {
+      await _open();
+    }
+  }
+
+  Future<Todo> insert(Todo todo) async {
+    await _init();
+    todo.id = await _streamDatabase.insert(tableTodo, todo.toMap());
+    return todo;
+  }
+
+  Stream<List<Todo>> getAll() async* {
+    await _init();
+
+    yield* _streamDatabase.createQuery(tableTodo,
+        columns: [columnId, columnDone, columnTitle],).mapToList((map) => Todo.fromMap(map));
+  }
+
+  Future close() async => _db.close();
+}
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'TodoProvider.dart';
+import 'dart:async';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  TodoProvider _todoProvider = TodoProvider();
+
+  void _createTodoAndIncrementCounter() {
+    _counter++;
+
+    _todoProvider.insert(Todo.fromMap({"title": "Todo number $_counter"}));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: StreamBuilder(
+          stream: _todoProvider.getAll(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              if (snapshot.data.length > 0) {
+                return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    return Text(snapshot.data[index].title);
+                  },
+                );
+              } else {
+                return Text("There are no todos.");
+              }
+            } else if (snapshot.hasError) {
+              return Text(snapshot.error.toString());
+            }
+
+            return CircularProgressIndicator();
+          },
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _createTodoAndIncrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
